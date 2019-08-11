8b54b2bb05df5bca6e1b38b75f69158dfa1a27f0
Jeff Whitaker <jswhit@fastmail.fm>
2019-08-11 10:47:32 -0600

    update

--- utils/filterbyreceipt.py
+++ utils/filterbyreceipt.py
@@ -13,14 +13,19 @@ ap.add_argument("input_bufr", help="path to input BUFR file")
 ap.add_argument("output_bufr", help="output BUFR file")
 msg="""
 threshold receipt time (YYYYMMDDHHMM). If not specifed,
-use maximum value in input_bufr."""
+use maximum value specified by --reference_bufr/-rb."""
 ap.add_argument("--receipt_time", '-rt', help=msg)
+msg="""
+bufr file to determine threshold receipt time.
+Use maximum value in file."""
+ap.add_argument("--reference_bufr", '-rb', help=msg)
 
 MyArgs = ap.parse_args()
 
 filename_in = MyArgs.input_bufr
 filename_out = MyArgs.output_bufr
-receipt_time_cutoff = int(MyArgs.receipt_time)
+receipt_time_cutoff = MyArgs.receipt_time
+filename_ref = MyArgs.reference_bufr
 
 if filename_out == filename_in:
     msg="output file must not have same name as input file"
@@ -29,27 +34,36 @@ if filename_out == filename_in:
 if receipt_time_cutoff is None:  # no receipt time specified
     # if filenameref is a file, determine receipt_time_cutoff
     # by looking for newest receipt time in the file.
-    bufr = ncepbufr.open(filename_in)
+    try:
+        bufr = ncepbufr.open(filename_ref)
+    except (IOError,TypeError):
+        msg = 'must specify either receipt_time or reference_bufr'
+        raise ValueError(msg)
     receipt_time_cutoff = -1
     while bufr.advance() == 0: # loop over messages.
         if bufr.receipt_time > receipt_time_cutoff:
             receipt_time_cutoff=bufr.receipt_time
     bufr.close()
+else:
+    receipt_time_cutoff = int(receipt_time_cutoff)
 print('receipt time cutoff = %s' % receipt_time_cutoff)
 
 bufr = ncepbufr.open(filename_in)
 bufrout = ncepbufr.open(filename_out,'w',bufr)
-nmsg=0; nmsgo=0
+nmsg=0; nmsgo=0; nmiss=0
 receipt_times=[]
 while bufr.advance() == 0: # loop over messages.
-    if bufr.receipt_time != -1 and  bufr.receipt_time > receipt_time_cutoff:    
+    if bufr.receipt_time < 0:
+        nmiss += 1
+    elif bufr.receipt_time > receipt_time_cutoff:
         # keep this bufr message, write out to file.
         bufrout.copy_message(bufr)
         receipt_times.append(bufr.receipt_time)
         nmsgo+=1
     nmsg+=1
-print('%s messages (out of %s) written to %s' % (nmsgo,nmsg,filenameo))
+print('%s messages (out of %s) written to %s' % (nmsgo,nmsg,filename_out))
+print('%s messages missing receipt time, skipped' % nmiss)
 receipt_times=np.array(receipt_times)
-if nmsgo: print('min/max receipt time: %s %s' % (receipt_times.min(), receipt_times.max()))
+if nmsgo: print('min/max output receipt time: %s %s' % (receipt_times.min(), receipt_times.max()))
 bufr.close()
 bufrout.close()
