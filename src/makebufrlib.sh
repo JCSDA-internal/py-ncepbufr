aca588440366945498bbb6e175daba867d5bd933
Jeff Whitaker <jswhit@fastmail.fm>
2019-10-18 12:08:45 -0600

    fix for issue #27

--- src/makebufrlib.sh
+++ src/makebufrlib.sh
@@ -1,5 +1,5 @@
-export FC="gcc -O2 -fPIC"
-export CC="gfortran -O2 -fPIC -DUNDERSCORE"
+export CC="gcc -O2 -fPIC"
+export FC="gfortran -O2 -fPIC -DUNDERSCORE"
 /bin/rm -f *.o *.a *.mod
 $CC -c `./getdefflags_C.sh` *.c
 $FC -c `./getdefflags_F.sh` modv*.F moda*.F `ls -1 *.F *.f | grep -v "mod[av]_"`
